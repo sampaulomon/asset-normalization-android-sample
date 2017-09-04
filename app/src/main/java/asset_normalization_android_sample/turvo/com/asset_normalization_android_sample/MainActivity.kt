e49e838f31fa82978e6a8aae2f38316d6551e4fe
Sam Solomon <sam.p@turvo.com>
2017-09-04 17:29:17 +0530

    Initial Commit.

---
+++ app/src/main/java/asset_normalization_android_sample/turvo/com/asset_normalization_android_sample/MainActivity.kt
@@ -0,0 +1,12 @@
+package asset_normalization_android_sample.turvo.com.asset_normalization_android_sample
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
